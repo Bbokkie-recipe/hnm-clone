a0c1810d8dd9c40b196f8a3150bf46a2af6af06e
Bbokkie-recipe <rbbokkie@gmail.com>
2022-12-11 23:44:09 +0900

    add route setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
 
+//1. 전체 상품 페이지, 로그인, 상품 상세 페이지
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route path="" element={<ProductAll />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/product/:id" element={<ProductDetail />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
