5a707a8bedef7abe4967cc11903c130286315959
Bbokkie-recipe <rbbokkie@gmail.com>
2022-12-27 22:15:39 +0900

    load product list

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import ProductCard from '../component/ProductCard'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const ProductAll = () => {
+	const [productList, setproductList] = useState([]);
+	const getProduct = async () =>{
+		let url = `http://localhost:5000/products`;
+		let response = await fetch(url);
+		let data = await response.json();
+		setproductList(data);
+	}
+	useEffect(()=>{
+		getProduct();
+	},[]);
 	return (
-		<div>ProductAll</div>
+		<div>
+			<Container>
+				<Row>
+			{productList.map((product) =>(
+				<Col lg={3}><ProductCard item={product}/>
+				</Col>
+			))}
+			</Row>
+			</Container>
+			</div>
 	)
 }
 
